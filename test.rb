74cb2000799358c5fe94f70284da06b5507a8d4d
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-01 11:47:38 +0900

    一覧表示と番号検索の実装

--- test.rb
+++ test.rb
@@ -2,7 +2,29 @@ require 'mysql2'
 
 client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "password", :encoding => "utf8", :database => "testDB")
 
-results = client.query('select * from books')
-results.each do |result|
-    puts result
+puts <<~eof
+実行したい操作を選択してください。
+1.データ一覧を表示
+2.本の番号で検索する
+eof
+action = gets.chomp.to_i
+
+case action
+when 1 # 登録データの一覧検索結果
+    results = client.query('select * from books')
+    puts '|番号|   タイトル   | 著者 |  発売日  |カテゴリー| 価格 |在庫|'
+    puts '--------------------------------------------------------------'
+    results.each do |result|
+        puts "| #{result["book_id"]} | #{result["title"]} | #{result["author"]} | #{result["date"]} |  #{result["cat_id"] } | #{result["price"] } | #{result["stock"] } |"
+    end
+when 2 # 本の番号検索
+    puts "本の番号を入力してください"
+    book_id_search = gets.chomp.to_i
+    statement = client.prepare('select * from books where book_id = ?')
+    results = statement.execute(book_id_search)
+    results.each do |row| 
+    puts row # {"id"=>1, "dep"=>1, "name"=>"hoge"}
+    end
+else
+    puts "error:入力値が不正です"
 end
