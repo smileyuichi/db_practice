92c2fbb08d7b7b200e46f75f975473507e897a89
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-12 13:19:11 +0900

    食材テーブルから食材を探して配列に格納する

--- cuisine.rb
+++ cuisine.rb
@@ -18,6 +18,8 @@ end
 puts <<~EOF
 実行したい処理を選択して下さい。
 --------------------------
+1.料理を一覧表示する
+2.新しい料理を追加する
 EOF
 
 choice = gets.chomp.to_i
@@ -25,4 +27,34 @@ choice = gets.chomp.to_i
 case choice
 when 1
     list_view(client)
+when 2
+    puts "料理名は？"
+    cuisine_name = gets.chomp
+    puts "使用食材は？"
+    client.query("select name from food").each do |food|
+        puts food["name"]
+    end
+    # 使用食材を配列に格納
+    food_names = []
+    # デリミターの設定
+    end_key = "."
+    # 食材名を配列food_namesに入れていく処理
+    food_name = gets.chomp
+    while end_key != food_name
+        # 食材テーブルに使用食材があるか確認、なければ入力を破棄
+        answers = client.query("select name from food where name = '#{food_name}'")
+        if answers.count != 0
+            answers.each do |ans|
+                food_names << food_name
+            end
+        else
+            puts "食材テーブルにデータがありません。"    
+        end
+        food_name = gets.chomp
+    end
+    
+    p food_names
+    # p results = client.query("insert into cuisine(name,cat_id) values('#{cuisine_name}',1)")
+    # p client.affected_rows
+    puts "登録が完了しました。"
 end
